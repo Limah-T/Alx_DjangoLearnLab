867bf0cbca1ae13d0119d37836d24fd46ca9ca2a
Limah-T <limahtechnology@yahoo.com>
2025-02-22 17:38:47 +0100

    TASK 0: Created an app and models table to perform various table relationships.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,26 +1,25 @@
 from .models import Book, Author, Library, Librarian
 
-# Query all books by a specific author.
 author = Author.objects.create(name="Limah")
 author.save()
 book = Book.objects.create(title="Python", author=author)
 book.save()
 book = Book.objects.create(title="Flask", author=author)
 book.save()
+library = Library.objects.create(name = "HardCopySections", book = book)
+library.save()
+librarian = Librarian(name = "Palmer")
+librarian.save()
+
+# Query all books by a specific author.
 book = Book.objects.filter(author=1)
 print(book.values())
 
 # List all books in a library.
-books = Book.objects.all()
-print(books)
+books = Library.objects.get(name=library)
+books.all()
 
 # Retrieve the librarian for a library.
-library = Library.objects.create(name = "HardCopySections")
-library.save()
-
-librarian = Librarian(name = "Palmer")
-librarian.save()
-
 librarian = Librarian.objects.filter(library=librarian)
 print(librarian)
 
