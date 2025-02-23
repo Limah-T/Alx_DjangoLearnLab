a8200c975782e407f0a79129ff8c0ed977788b77
Limah-T <limahtechnology@yahoo.com>
2025-02-23 15:13:43 +0100

    Task2: Define templates and pass related views and urls.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 
 # Create your views here
@@ -20,7 +21,7 @@ class LibraryDetailView(DetailView):
     
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy(login)
     template_name = 'relationship_app/register.html'
 
 class LoginView(CreateView):
