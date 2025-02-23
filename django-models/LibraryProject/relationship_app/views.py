4b3f00fc5f1067def3155be70ee34cf069c3e09d
Limah-T <limahtechnology@yahoo.com>
2025-02-23 18:11:34 +0100

    TASK3: User model and creation of roles.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -51,7 +51,7 @@ def is_member(user):
 
 @login_required
 @user_passes_test(is_admin)
-def librarian_view(request):
+def admin_view(request):
     return render(request, 'admin_view')
 
 @login_required
@@ -61,7 +61,7 @@ def librarian_view(request):
 
 @login_required
 @user_passes_test(is_member)
-def librarian_view(request):
+def member_view(request):
     return render(request, 'member_view')
 
 
