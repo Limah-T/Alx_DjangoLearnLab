2ff5017df026c682c0ac48980ee6525c8ddd3615
Limah-T <limahtechnology@yahoo.com>
2025-02-23 13:17:33 +0100

    TASK 1: Created function based view and class based view, then passed argument for templates to display.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import LibraryDetailView, list_books
+from .views import LibraryDetailView
+from .views import list_books
+
 urlpatterns = [
     path("books/", list_books, name='list_books'),
     path("library/<int:id>", LibraryDetailView.as_view(), name='library')
