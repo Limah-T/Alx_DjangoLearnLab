68180336e17165b1c0865dc2d39dff9a5aa5fcc1
Limah-T <limahtechnology@yahoo.com>
2025-02-23 15:24:37 +0100

    Task2: Define templates and pass related views and urls.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("books/", list_books, name='list_books'),
     path("library/<int:pk>", LibraryDetailView.as_view(), name='library'),
     path("register/", views.register, name='register'),
-    path("login/", views.LoginView.as_view(template_name = 'relationship_app.login.html'), name='login'),
-    path("logout/", views.LogoutView.as_view(template_name='relationship_app.logout.html') ,name="logout")
+    path("login/", views.LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path("logout/", views.LogoutView.as_view(template_name='relationship_app/logout.html') ,name="logout")
 ]
