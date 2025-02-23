978892db0634597b13351e2d73ca2bbd811a416d
Limah-T <limahtechnology@yahoo.com>
2025-02-23 20:51:40 +0100

    TASK3: User model and creation of roles.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('member-view/', member_view, name='member_view'),
 
       # Secured URLs
-        path('add_book/', views.add_book, name='add_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 
@@ -25,28 +25,3 @@ urlpatterns = [
     # URL pattern for the class-based view (LibraryDetailView)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
-
-# from django.urls import path
-# from .views import LibraryDetailView
-# from .views import list_books, admin_view, librarian_view, member_view
-# from django.contrib.auth import views
-# from . import views
-
-# # SignUpView, register
-
-# urlpatterns = [
-#     path("books/", list_books, name='list_books'),
-#     path("library/<int:pk>", LibraryDetailView.as_view(), name='library'),
-#     path("register/", views.register, name='register'),
-#     path("login/", views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-#     path("logout/", views.LogoutView.as_view(template_name='relationship_app/logout.html') ,name="logout"),
-
-#     path('admin-view/', admin_view, name='admin_view'),
-#     path('librarian-view/', librarian_view, name='librarian_view'),
-#     path('member-view/', member_view, name='member_view'),
-
-#       # Secured URLs
-#     path('add_book/', views.add_book, name='add_book'),
-#     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
-#     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
-# ]
