aa8385fe77ddcd7a37d3c4a14c08cb68cfe83ef6
Limah-T <limahtechnology@yahoo.com>
2025-02-23 13:14:53 +0100

    TASK 1: Created function based view and class based view, then passed argument for templates to display.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Library
 from .models import Book
-from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", context={'books': books})
 
-class ListBookView(ListView):
+class ListBookView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
