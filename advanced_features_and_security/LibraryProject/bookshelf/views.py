97dab89f13f3482c88bbcb4915062e8c6fbd03b0
Limah-T <limahtechnology@yahoo.com>
2025-02-28 18:29:30 +0100

    Managing Permissions and Groups in Django.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,25 +3,24 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
-@permission_required('bookshelf.can_create_book', raise_exception=True)
+@permission_required('bookshelf.can_view_book', raise_exception=True)
 def book_list(request):
     all_books = [
         {'id': 1, 'title': 'Python', 'author': 'Guido Van Rossum'},
         {'id': 2, 'title': 'Django', 'author': 'Guido Van Rossum'},
         {'id': 3, 'title': 'Flask', 'author': 'Guido Van Rossum'}
     ]
-    return render(request, 'bookshelf/create.html', {'all_books': all_books})
+    return render(request, 'bookshelf/view.html', {'all_books': all_books})
 
-@permission_required('bookshelf.can_view', raise_exception=True)
+@permission_required('bookshelf.can_create_book', raise_exception=True)
 def edit_view(request):
-    
-    return render(request, 'bookshelf/view.html')
+    return render(request, 'bookshelf/create.html')
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
 def edit_view(request):
     return render(request, 'bookshelf/edit.html')
 
-@permission_required('bookshelf.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
 def edit_view(request):
     return render(request, 'bookshelf/delete.html')
 
