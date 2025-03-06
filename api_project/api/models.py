07d23c67a613121576197151b21c1771ed263875
Limah-T <limahtechnology@yahoo.com>
2025-03-06 17:08:46 +0100

    Week12 - Task1: Building Your First API Endpoint with Django REST Framework.

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -6,6 +6,5 @@ class Book(models.Model):
     author = models.CharField(max_length=20, null=False, blank=False)
 
     def __str__(self):
-        return self.title, "by", self.author
-    
+        return f"{self.title} by {self.author}"
     
