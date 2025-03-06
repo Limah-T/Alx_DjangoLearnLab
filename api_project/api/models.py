baf8dc590c30a20f6f7976abfeacef8186dfb9cf
Limah-T <limahtechnology@yahoo.com>
2025-03-06 16:52:04 +0100

    Week12 - Task 0: Setting Up a New Django Project with Django REST Framework

---
+++ api_project/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=15, null=False, blank=False)
+    author = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.title, "by", self.author
+    
+    
