373832ec99f849fac73101150bbc2cdb47017744
Limah-T <limahtechnology@yahoo.com>
2025-03-27 03:47:46 +0100

    Implementing Posts and Comments Functionality.

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,18 +1,26 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'post', views.PostView)
+router.register(r'comment', views.CommentView)
 
 urlpatterns = [
-# """Post API Endpoints"""
-    path('post/create', views.CreateView.as_view()),
-    path('posts', views.ListView.as_view()),
-    path('post/<int:id>', views.RetrieveView.as_view()),
-    path('post/<int:id>/update', views.UpdateView.as_view()),
-    path('post/<int:id>/delete', views.DeleteView.as_view()),
+# """Post API Endpoints for Generic View"""
+    # path('post/create', views.CreateView.as_view()),
+    # path('posts', views.ListView.as_view()),
+    # path('post/<int:id>', views.RetrieveView.as_view()),
+    # path('post/<int:id>/update', views.UpdateView.as_view()),
+    # path('post/<int:id>/delete', views.DeleteView.as_view()),
 
-#   """Comment API Endpoints"""
-    path('comment/create', views.CommentCreateView.as_view()),
-    path('comments', views.CommentListView.as_view()),
-    path('comment/<int:id>', views.CommentRetrieveView.as_view()),
-    path('comment/<int:id>/update', views.CommentUpdateView.as_view()),
-    path('comment/<int:id>/delete', views.CommentDeleteView.as_view()),
+    # Endpoints for ModelViewset
+    path('', include(router.urls)),
+    
+#   """Comment API Endpoints for Generic View"""
+    # path('comment/create', views.CommentCreateView.as_view()),
+    # path('comments', views.CommentListView.as_view()),
+    # path('comment/<int:id>', views.CommentRetrieveView.as_view()),
+    # path('comment/<int:id>/update', views.CommentUpdateView.as_view()),
+    # path('comment/<int:id>/delete', views.CommentDeleteView.as_view()),
 ]
